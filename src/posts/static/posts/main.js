b57cadf2e9bbf1ffeff8bf72ad60cecfab4a5dd8
Liam Howatt <Lhowatt8138@conestogac.on.ca>
2025-03-29 15:53:13 -0400

    Added posts vua ajax and spinner to main page

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,7 +1,8 @@
 console.log('hello world');
 
 const helloWorldBox = document.getElementById('hello-world');
-
+const postsBox = document.getElementById('posts-box');
+const spinnerBox = document.getElementById('spinner-box');
 
 $.ajax({
     type: 'GET',
@@ -13,4 +14,25 @@ $.ajax({
     error: function(error) {
         console.log('error',error);
     }
-})
+});
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log(response);
+        const data = response.data
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - <b>${el.body}<b/><br>
+                `
+            });
+        }, 100)
+    },
+    error: function(error) {
+        console.log(error);
+    }
+});
