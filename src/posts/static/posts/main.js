70e55849c2c6cfe3ad1dd3b1aba1fad1d785b446
Liam Howatt <Lhowatt8138@conestogac.on.ca>
2025-03-31 16:57:31 -0400

    Added dropzone to add post creation

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -12,7 +12,10 @@ const csrf = document.getElementsByName('csrfmiddlewaretoken');
 const url = window.location.href
 
 const alertBox = document.getElementById('alert-box');
-console.log('csrf', csrf[0].value)
+
+const dropzone = document.getElementById('my-dropzone');
+const addBtn = document.getElementById('add-btn');
+const closeBtn = [...document.getElementsByClassName('add-modal-close')]
 
 const getCookie = (name) => {
     let cookieValue = null;
@@ -88,7 +91,7 @@ const getData = () => {
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
-                                            <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})` : `Like (${el.count})`}</button>
+                                            <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})` : `Like (${el.count})`}</button>
                                         </form>
                                     </div>
                                 </div>
@@ -119,6 +122,8 @@ loadBtn.addEventListener('click', () => {
     getData();
 })
 
+
+let newPostId = null
 postForm.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -132,6 +137,7 @@ postForm.addEventListener('submit', e => {
         },
         success: function(response) {
             console.log(response)
+            newPostId = response.id
             postsBox.insertAdjacentHTML('afterbegin', `
                 <div class="card mb-2">
                     <div class="card-body">
@@ -141,11 +147,11 @@ postForm.addEventListener('submit', e => {
                     <div class="card-footer">
                         <div class="row">
                             <div class="col-2">
-                                <a href="#" class="btn btn-primary">Details</a>
+                                <a href="${url}${response.id}" class="btn btn-primary">Details</a>
                             </div>
                             <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${response.id}">
-                                    <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                    <button class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
                                 </form>
                             </div>
                         </div>
@@ -153,15 +159,40 @@ postForm.addEventListener('submit', e => {
                 </div>
             `)
             likeUnlikePosts()
-            $('#addPostModal').modal('hide')
+            // $('#addPostModal').modal('hide')
             handleAlerts('success', 'New post added!')
-            postForm.reset()
+            // postForm.reset()
         },
         error: function(error) {
             console.log(error)
             handleAlerts('danger', 'Oops.. something went wrong')
         }
     })
+})
+
+addBtn.addEventListener('click', () => {
+    dropzone.classList.remove('not-visible')
+})
+
+closeBtn.forEach(btn => btn.addEventListener('click', () => {
+    postForm.reset()
+    if (!dropzone.classList.contains('not-visible')) {
+        dropzone.classList.add('not-visible')
+    }
+}))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone('#my-dropzone', {
+    url: 'upload/',
+    init: function() {
+        this.on('sending', function(file, xhr, formData) {
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 5,
+    maxFilesize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
 })
 
 getData();
